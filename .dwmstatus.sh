41fc3ef0b62526f45ff3953552d5d3249efd43d7
Daniel Grossmann-Kavanagh <me@danielgk.com>
2019-02-25 11:24:52 +1100

    notify on low battery

--- .dwmstatus.sh
+++ .dwmstatus.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 while true; do
-    battery=$(acpi -b | cut -d"," -f2 | cut -d" " -f2)
+    battery=$(acpi -b | grep -v 'rate information unavailable' | cut -d"," -f2 | cut -d" " -f2)
+    charge_state=$(acpi -b | grep -v 'rate information unavailable' | grep "Discharging"; echo "$?")
+    batt_pct=$(acpi -b | grep -v 'rate information unavailable' | grep 'Discharging' | sed 's/^.*Discharging.*\([ 0-9][0-9]\)%.*$/\1/g')
+    if [[ ! -z $batt_pct ]]; then
+        if [[ $batt_pct < 40 ]]; then
+            notify-send 'low on juice' 'PLUG ME IN'
+        fi
+    fi
     ip=$(ip addr  show dev wlp58s0 | grep 'inet ' | awk '{print $2}')
     ip="wlp58s0:${ip}"
     xsetroot -name " $battery  $ip  $(date +"%r") ($(date -u +"%Y-%m-%dT%H:%MZ"))"
