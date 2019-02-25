72e77a589ba3a7832ba66e374ab78f2058fa2e0d
Daniel Grossmann-Kavanagh <me@danielgk.com>
2019-02-25 12:23:35 +1100

    add aliases for new monitor (xps, wide)

---
+++ .screenlayout/wide.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DP-1 --mode 3440x1440 --scale 1.5x1.5 --pos 0x0 --panning 5160x2160 --rotate normal --output eDP-1 --off --output DP-2 --off
+feh --bg-fill ~/pictures/PIA14934_saturn.jpg
